e7d40115b41e94ff4a5492624b0dc28e8de8df25
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 21:25:28 +0300

    Add a date post function

--- pichblog.py
+++ pichblog.py
@@ -11,19 +11,19 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True, nullable=false)
-    email= db.Column(db.String(200),unique=True, nullable=false)
-    image_file= db.Column(db.String(),nullable=false,default='default.jpg')
-    password=db.Column(db.String(60),nullable=false)
+    username = db.Column(db.String(20),unique=True, nullable=False)
+    email= db.Column(db.String(200),unique=True, nullable=False)
+    image_file= db.Column(db.String(),nullable=False,default='default.jpg')
+    password=db.Column(db.String(60),nullable=False)
     
     def __repr__(self):
         return f"User('{self.username}','{self.email},'{self.image_file}')"
     
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20), nullable=false)
-  
-     
+    title= db.Column(db.String(180), nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False,default= datetime.utcnow())
+    
     
 
     
