af6f96ed299b01f7ae6f97f6392270cc01116d49
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 13:33:11 +0300

    Add a template folder and create  home.htm

--- pichblog.py
+++ pichblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, 
 
 app = Flask(__name__)
 
@@ -27,7 +27,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template( title='About')
+    return render_template(, title='About')
 
 
 
