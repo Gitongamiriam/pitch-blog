d67e59b010659ee529fc05ec95dde365ec2e1fd4
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 14:36:01 +0300

    add the login logic in the template

--- pichblog.py
+++ pichblog.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='c4bd279f9afbb8'
 
 
 posts = [
@@ -31,7 +32,26 @@ def about():
     return render_template('about.htm', title='About')
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.htm', title='Register', form=form)
 
-if __name__== '__main__':
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.htm', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-    
