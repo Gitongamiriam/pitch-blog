46194b1bea0e5dd8ee62e4e4fac13352a25b39ba
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 20:44:30 +0300

    Intergrate a database

--- pichblog.py
+++ pichblog.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask-sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] ='c4bd279f9afbb8'
+app.config['SQLALCHEMY_DATABASE_URL']='sqlite:///pich.db'
 
+db = SQLAlchemy(app)
 
 posts = [
     {
