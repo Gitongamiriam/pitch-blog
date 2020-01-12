09165111cc0d97ef0ae7edf915e454eab4604593
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 21:29:13 +0300

    Add a content function

--- pichblog.py
+++ pichblog.py
@@ -23,6 +23,7 @@ class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title= db.Column(db.String(180), nullable=False)
     date_posted = db.Column(db.DateTime,nullable=False,default= datetime.utcnow())
+    content = db.Column(db.Text,nullable = False)
     
     
 
