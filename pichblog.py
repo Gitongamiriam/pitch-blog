d849cf1f66de58f880669d5418c9b1dae04308e5
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-12 13:30:53 +0300

    Create a post instance

--- pichblog.py
+++ pichblog.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Home Page"
+    return render_template(, posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "About Page"
+    return render_template( title='About')
+
 
 
 if __name__== '__main__':
