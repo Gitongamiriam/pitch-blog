858a9175bebfc24b5e262fc75b953760018e28d0
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-14 09:36:31 +0300

    Add a route for posting new quotes

--- pichblog/routes.py
+++ pichblog/routes.py
@@ -105,3 +105,6 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.htm', title='Account',
                            image_file=image_file, form=form)
+@app.route("/post/new")
+@login_required
+def new_post():    
