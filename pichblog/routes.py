580f2d423a09bb2758e1eb5105cf4412bc928718
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-14 11:10:12 +0300

    Add a route for post

--- pichblog/routes.py
+++ pichblog/routes.py
@@ -102,3 +102,8 @@ def new_post():
         flash('Your post has been created','success')
         return redirect(url_for('home'))
     return render_template('create_post.htm', title='New Post',form=form)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.htm',title=post.title , post=post)
