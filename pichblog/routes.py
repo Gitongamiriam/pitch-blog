2524571aae8f38e513a0142df3de43b4eaea45a1
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-14 10:53:57 +0300

    Add a function to post a quote

--- pichblog/routes.py
+++ pichblog/routes.py
@@ -8,25 +8,11 @@ from pichblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.htm', posts=posts)
 
 
@@ -105,11 +91,14 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.htm', title='Account',
                            image_file=image_file, form=form)
-@app.route("/post/new",methods=['GET', 'POST')
+@app.route("/post/new",methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
-     if form.validate_on_submit():
-         flash('Your post has been created','success')
-         return redirect(url_for('home'))
+    if form.validate_on_submit():
+        post= Post(title=form.title.data,content=form.content.data,author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created','success')
+        return redirect(url_for('home'))
     return render_template('create_post.htm', title='New Post',form=form)
